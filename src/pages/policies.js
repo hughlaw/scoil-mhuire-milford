3720855b53459346ba30a3c7f4956b9dc227547c
Hugh Law <hugh.law@terminalfour.com>
2020-10-10 19:02:49 +0100

    allow policy document to be optional without breaking build

--- src/pages/policies.js
+++ src/pages/policies.js
@@ -6,6 +6,11 @@ import { graphql, useStaticQuery } from 'gatsby';
 import BlockContent from '@sanity/block-content-to-react';
 import { FiDownload } from 'react-icons/fi';
 import { serializers } from '../blockContent';
+import styled from 'styled-components';
+
+const Hr = styled.hr`
+  width: 100%;
+`;
 
 const PoliciesPage = () => {
   const { sanityPoliciesPage: policyPage } = useStaticQuery(graphql`
@@ -45,22 +50,25 @@ const PoliciesPage = () => {
 
         <Row className="mt-4 mb-4">
           <Col>
-            {policyPage.policies.map((policy, i) => (
+            {policyPage.policies.map((policy, i, arr) => (
               <Row className="mb-4" key={`policy-${i}`}>
                 <Col className="d-sm-flex justify-content-between align-items-center mb-3">
                   <h2 className="mb-0">{policy.name}</h2>
-                  <a
-                    className="btn btn-secondary"
-                    href={`${policy.policyDocument.asset.url}?dl=`}
-                  >
-                    <FiDownload /> Download
-                    <span className="sr-only"> {policy.name} document</span> (
-                    {Math.ceil(policy.policyDocument.asset.size / 1024)}kB)
-                  </a>
+                  {policy.policyDocument && (
+                    <a
+                      className="btn btn-secondary"
+                      href={`${policy.policyDocument.asset.url}?dl=`}
+                    >
+                      <FiDownload /> Download
+                      <span className="sr-only"> {policy.name} document</span> (
+                      {Math.ceil(policy.policyDocument.asset.size / 1024)}kB)
+                    </a>
+                  )}
                 </Col>
                 <Col xs={12}>
                   <p>{policy.description}</p>
                 </Col>
+                {arr.length !== i + 1 && <Hr />}
               </Row>
             ))}
           </Col>
